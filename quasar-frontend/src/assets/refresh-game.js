95b4227c166340ffd90ae27fdfc9fafaee3af625
Mike Casto <castoware@gmail.com>
2025-10-06 10:51:38 -0500

    Added refresh-game function

--- quasar-frontend/src/assets/refresh-game.js
+++ quasar-frontend/src/assets/refresh-game.js
@@ -5,7 +5,20 @@ import { Notify } from "quasar";
 export default async () => {
   const store = useStore();
 
-  if (!store.game) {
+  const response = await callApi({
+    path: "/game",
+    method: "get",
+    useAuth: true,
+  });
+
+  if (response.status != "success") {
+    Notify.create({ type: "negative", message: response.message });
+    return;
+  }
+
+  store.game = response.game;
+
+  setInterval(async () => {
     const response = await callApi({
       path: "/game",
       method: "get",
@@ -18,22 +31,5 @@ export default async () => {
     }
 
     store.game = response.game;
-  }
-
-  setInterval(async () => {
-    if (!store.game) {
-      const response = await callApi({
-        path: "/game",
-        method: "get",
-        useAuth: true,
-      });
-
-      if (response.status != "success") {
-        Notify.create({ type: "negative", message: response.message });
-        return;
-      }
-
-      store.game = response.game;
-    }
   }, 1000);
 };
